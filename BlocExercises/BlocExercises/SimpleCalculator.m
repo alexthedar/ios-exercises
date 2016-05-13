73af584f9f4bd90f9d6fc39013ebdc55eefb17b5
alexthedar <alex.castaneda@gmail.com>
2016-05-13 07:52:13 -0700

    all tests passing and code refactored

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,18 +11,20 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    /* WORK HERE */
-    return number;
+// used +1 instead of ++ operator because wanted to return answer without intermediary ++ step and simplify code
+    return number+1;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    /* WORK HERE */
-    return number1;
+    //return answer instead of intermediary calcuation step to simplify code
+    return number1=number1+number2;
+    
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    /* WORK HERE */
-    return 0;
+    //eliminated return 0 in order to simplify code
+    return dividend % divisor;
+
 }
 
 @end
