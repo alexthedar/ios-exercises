3666ccdab8dfeb08c4bc05fd9f18359659cab6da
alexthedar <alex.castaneda@gmail.com>
2016-05-17 08:05:23 -0700

    all tests passing

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,34 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    NSString *answer;
+    if (string1 == string2) {
+        answer = @"YES";
+    } else {
+        answer = @"NO";
+    }
+    return answer;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+    NSString *answer;
+    if ([number1 isEqualToNumber:number2]) {
+        answer = @"YES";
+    } else {
+        answer = @"NO";
+    }
+    return answer;
+
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    NSString *answer;
+    if (integer1 > integer2) {
+        answer = @"YES";
+    } else {
+        answer = @"NO";
+    }
+    return answer;
 }
 
 @end
