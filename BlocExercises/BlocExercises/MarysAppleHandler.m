202f21205644dc872f52ccd0d76e23f8bde734d8
alexthedar <alex.castaneda@gmail.com>
2016-05-20 07:40:25 -0700

    modified if/else to include below 4, 7 through 1000

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,18 +12,20 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
-    if (dollars == 4){
+    if (dollars <= 4){
         itemToReturn = @"get out of my store";
     } else if ( dollars == 5) {
         itemToReturn = @"have some gum";
     } else if (dollars == 6) {
         itemToReturn = @"have an apple";
-    } else if ( dollars >= 1000 && dollars < 999999999) {
+    } else if (dollars >= 7 && dollars < 1000){
+        itemToReturn = @"get out of my store";
+    }else if ( dollars >= 1000 && dollars < 1000000000) {
         itemToReturn = @"have an Apple computer";
     } else if (dollars >= 1000000000){
         itemToReturn = @"have The Big Apple";
     } else {
-        itemToReturn = @"no information given";
+        itemToReturn = @"test did not provide additional parameters";
     }
 
     
